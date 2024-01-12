6bd7534ef01e73575bc6b16208feb6c13dbde48b
Himel Islam <himel7100@gmail.com>
2024-01-12 20:20:59 +0600

    blog pages

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,40 @@
 
+const blogs = [
+  {
+    id : 1,
+    title : "Blog 1",
+    description : "blog description for one"
+  },
+  {
+    id : 2,
+    title : "Blog 2",
+    description : "blog description for two"
+  },
+  {
+    id : 3,
+    title : "Blog 3",
+    description : "blog description for three"
+  },
+  {
+    id : 4,
+    title : "Blog 4",
+    description : "blog description for four"
+  }
+]
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <div>
+      
+      <h2>Blog</h2>
+
+      <ul>
+          {
+            blogs.map(blog => <li>{blog.title}</li>)
+          }
+      </ul>
+      
+    </div>
+  
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       Hi test and ada a afljk adljadf lad f
+
+      <Link href="blog">Blog</Link>
     </div>
   )
 }
